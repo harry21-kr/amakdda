3cad55d979728bc8292a7bb2e37cca6d4e87f8d6
박원빈 <harry21@kakao.com>
2024-05-10 20:01:29 +0900

    (design) change site name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
           alignItems: "center",
         }}
       >
-        <h1 style={{ fontSize: 24, fontWeight: "bold" }}>a mat da</h1>
+        <h1 style={{ fontSize: 24, fontWeight: "bold" }}>amakdda</h1>
         <p>아 맞다! 하지말고 기록하세요.</p>
       </div>
     </header>
